6c8dc7e184fa9714faa0952948c7c91740f67f56
fredrik <nguyen.eszter@gmail.com>
2025-04-09 10:47:01 +0200

    storage mod

--- package_for_pipeline/cellreg_analysis.py
+++ package_for_pipeline/cellreg_analysis.py
@@ -16,6 +16,7 @@ with h5py.File('x:/cellRegistered_20250404_115825.mat', 'r') as file:
 cell_to_index_map = data['cell_registered_struct']['cell_to_index_map']
 cell_to_index_map = np.array(cell_to_index_map, dtype=int)'''
 #mat = io.loadmat('c:/Hyperstim/data_analysis/2023_09_25_GCAMP6F/merged_tiffs/cellreg_files/cellRegistered_20250404_115715.mat')
+cell_to_index_map = np.array(data['cell_registered_struct']['cell_to_index_map'], dtype=int)
 
 num_sessions, num_registered_cells = data.shape
 print(num_registered_cells, num_sessions)
@@ -44,5 +45,20 @@ for row in data:
 for i in range(num_sessions):
     for j in range(i+1, num_sessions):
         print(f'overlapping cells in session {i+1} & {j+1}: {session_pair_counts[i,j]}  ')
+
+'''
+for i in range(num_sessions):
+    for j in range(i + 1, num_sessions):
+        holder = []
+        #holder = 0
+
+        for row in cell_to_index_map:
+            if row[i] > 0 and row[j] > 0:
+                holder.append(True)  # or append row itself if you want to store matched neurons
+                #holder += 1
+        # Count how many matches were found for this session pair
+        num_matches = len(holder)
+        print(f"Sessions {i+1} & {j+1}: {num_matches} overlapping cells")
+'''
 
 

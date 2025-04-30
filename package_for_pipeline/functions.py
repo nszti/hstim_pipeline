aeca2dd3d17128d935a5e0a1e0cd843384dfd5a0
fredrik <nguyen.eszter@gmail.com>
2025-04-30 09:50:12 +0200

    grand avg for all rois all trials, save to .npy file

--- package_for_pipeline/functions.py
+++ package_for_pipeline/functions.py
@@ -1596,6 +1596,53 @@ def plot_stim_traces(expDir, frame_rate, num_repeats, num_stims_per_repeat, list
             plt.tight_layout()
             plt.savefig(avg_plot_path)
             plt.show()
+    # plot 4.2:
+
+            fig, ax = plt.subplots(figsize=(8, 5))
+            time = np.linspace(-1, 3, total_frames)
+            fig.suptitle("Overall average of trials per amplitude", fontsize=16)
+
+            sum_avg_per_amplitude = {}
+
+            for stim_idx, amplitude in enumerate(amplitude_values):
+                roi_traces = []
+                for roi_idx in cell_indices:
+                    is_activated_in_any_repeat = any(
+                        activation_results[roi_idx][repeat][stim_idx] == 1 for repeat in range(num_repeats))
+                    if not is_activated_in_any_repeat:
+                        continue
+                    roi_trials = []
+                    for repeat in range(num_repeats):
+                        if activation_results[roi_idx][repeat][stim_idx] == 1:
+                            roi_trials.append(all_traces[repeat, stim_idx])
+                    # avg across trials
+                    if roi_trials:
+                        roi_avg_trace = np.mean(roi_trials, axis=0)
+                        roi_traces.append(roi_avg_trace)
+                # avg across rois
+                if roi_traces:
+                    sum_avg = np.mean(roi_traces, axis=0)
+                    ax.plot(time, sum_avg, label=f"{amplitude} μA", linewidth=2)
+                    sum_avg_per_amplitude[amplitude] = sum_avg
+
+
+            # ax.axvline(x=0, color='gray', linestyle='--', label='Stim Onset')
+            ax.set_xlabel("Time (s)")
+            ax.set_ylabel("Mean ΔF/F₀")
+            ax.set_title("Averaged across all active rois")
+            ax.legend()
+            ax.grid(True)
+            ax.set_ylim(min_trace_value, max_trace_value)
+
+            avg_plot_path = os.path.join(expDir, dir, 'sum_avg_trace_per_amplitude.png')
+            plt.tight_layout()
+            plt.savefig(avg_plot_path)
+            plt.show()
+
+            # Save the sum_avg data to .npy
+            sum_avg_path = os.path.join(expDir, dir, 'sum_avg_values.npy')
+            np.save(sum_avg_path, sum_avg_per_amplitude)
+
 
 #scratch_1
 def scratch_val(tiff_dir):

6725b6fa19995b69b1eccca4c7fd4544d063850a
NP <nguyen.eszter@gmail.com>
2025-06-11 16:56:07 +0200

    clean up

--- pipeline_script.py
+++ pipeline_script.py
@@ -37,27 +37,44 @@ from package_for_pipeline import frequency_to_save
 #root_directory = 'c:/Hyperstim/data_analysis/2025-04-24-Amouse-invivo-GCaMP6f/'
 #root_directory = 'c:/Hyperstim/data_analysis/2025-04-01-Amouse-invivo-GCaMP6f/' #
 #root_directory = 'c:/Hyperstim/data_analysis/2023_09_25_GCAMP6F/'
-root_directory = 'c:/Hyperstim/data_analysis/2025-03-25-Amouse-invivo-GCaMP6f/'
+#root_directory = 'c:/Hyperstim/data_analysis/2025-03-25-Amouse-invivo-GCaMP6f/'
+#root_directory = 'e:/2024_10_03_GCamp6s_in_vivo_chronic_test_d106/'
+#root_directory = 'c:/Hyperstim/data_analysis/2024_02_15_chronic_in_vivo_test/'
+#root_directory = 'e:/2024_04_12_in_vivo_GCAMP6s_day6test_MUnit_1/'
+#root_directory = 'e:/2024_10_03_GCamp6s_in_vivo_chronic_test_day2025-05-21/'
 #root_directory = 'c:/Hyperstim/data_analysis/AMouse-2025-03-05-invivo-GCaMP6f/'
+root_directory = 'c:/Hyperstim/data_analysis/2024_09_17_GCamp6s_in_vivo/'
+#root_directory = 'e:/2025-05-26-Cmouse-invivo-Csabi/'
 #tiff_directory = 'c:/Hyperstim/data_analysis/2025-04-24-Amouse-invivo-GCaMP6f/merged_tiffs/'
 #tiff_directory = 'c:/Hyperstim/data_analysis/2025-04-01-Amouse-invivo-GCaMP6f/merged_tiffs/'
 #tiff_directory = 'c:/Hyperstim/data_analysis/2023_09_25_GCAMP6F/merged_tiffs/'
-tiff_directory= 'c:/Hyperstim/data_analysis/2025-03-25-Amouse-invivo-GCaMP6f/merged_tiffs/'
+#tiff_directory= 'c:/Hyperstim/data_analysis/2025-03-25-Amouse-invivo-GCaMP6f/merged_tiffs/'
+#tiff_directory= 'e:/2024_10_03_GCamp6s_in_vivo_chronic_test_d106/merged_tiffs/'
+#tiff_directory = 'e:/2024_04_12_in_vivo_GCAMP6s_day6test_MUnit_1/merged_tiffs/'
+#tiff_directory = 'e:/2024_10_03_GCamp6s_in_vivo_chronic_test_day2025-05-21/merged_tiffs/'
+#tiff_directory ='e:/2024_04_12_in_vivo_GCAMP6s_day6test_MUnit_1/merged_tiffs/'
 #tiff_directory = 'c:/Hyperstim/data_analysis/AMouse-2025-03-05-invivo-GCaMP6f/merged_tiffs/'
+tiff_directory = 'c:/Hyperstim/data_analysis/2024_09_17_GCamp6s_in_vivo/merged_tiffs/'
+#tiff_directory = 'e:/2025-05-26-Cmouse-invivo-Csabi/merged_tiffs/'
 #mesc_file_name = 'AMouse-2025-03-05-invivo-GCaMP6f-2' '2025-04-01-Amouse-invivo-GCaMP6f-2'
 #mesc_file_name = '2025-03-25-Amouse-invivo-GCaMP6f'
 #mesc_file_name = 'AMouse-2025-03-05-invivo-GCaMP6f-2'
 #mesc_file_name = '2025-04-01-Amouse-invivo-GCaMP6f-2'
-mesc_file_name ='2025-04-24-Amouse-invivo-GCaMP6f'
+mesc_file_name ='2024_09_17_GCamp6s_2__in_vivo'
 mesc_DATA_file = 'mesc_data.npy' #from mesc_tiff_extract
-mat_file = 'cellRegistered_20250505_175342.mat'
-postfix = 'sum_avg_dir/activated_roi_masks/'
+mat_file = 'cellRegistered_20250523_110314.mat'
+postfix = ''
 list_of_file_nums = [
-  [15]
+  [15,16,17,18,19]
+  #[20,21, 22 ,23,24,25,26,27,28,29,30]
+  #[33,34,35,36,37,38,39,40,41,42,43]
+  #[46,47,48,49,50,51,52,53,54,55,56],
+  #[8,9,10,11,12,13,14,15,16,17,18]
+  #[46]
 
 ]
 
-gcamp = 'f' #for GCaMP6s: 's'
+gcamp = 's' #for GCaMP6s: 's'
 stim_type = 'amp' # 'freq', 'pulse_dur',  'amp'
 '''
 #root_directory = 'c:/Hyperstim/data_analysis/2025-04-15-Amouse-invivo-GCaMP6f/' #
@@ -84,17 +101,18 @@ gcamp = 'f' #for GCaMP6s: 's'
 stim_type = 'amp' # 'freq', 'pulse_dur',  'amp'
 '''
 RUN_MESC_PREPROCESS = False  #tiff extraction
-RUN_PREPROCESS = False # osszefuz listaban megadott tifeket
+RUN_PREPROCESS = True # osszefuz listaban megadott tifeket
 S2P = False #suite2p futtatás
 #--------
-RUN_ANALYSIS_PREP = False  #F0 savelodik, ha modositod a suite2p barmelyik propertyet, akkor ezt ujra kell futtatni h frissuljon az F0
+RUN_ANALYSIS_PREP = True  #F0 savelodik, ha modositod a suite2p barmelyik propertyet, akkor ezt ujra kell futtatni h frissuljon az F0
 PLOTS = False #Analysis plotok, utolso 3 a relevans
 PLOT_BTW_EXP = False
 RUN_FOR_SINGLE_PARAM_EXP = False
 RUN_FOR_ADVANCED_EXP = False
 RUN_CELLREG_PREP = False #cellreghez mat fileokat ment ki
 #cellreg hasznalat: CellReg.m run > GUIban load new dataval berakod a mat fileokat & 1.07 micront megadod> Non-rigid alignment futtatas > 12 micronos probabilistc modeling futtatas > a tobbit csak megnyomkodod sorban
-RUN_CELLREG_ANALYSIS = True
+RUN_CELLREG_ANALYSIS = False
+VIDEO = False
 
 #------VALUES TO CHANGE END------
 if RUN_MESC_PREPROCESS:
@@ -103,7 +121,7 @@ if RUN_MESC_PREPROCESS:
 #-----1.2.step: frequency_to_save, electrode_roi_to_save-->automatization pending-----
 if RUN_PREPROCESS:
   #frequency_to_save.frequency_electrodeRoi_to_save(root_directory, tiff_directory)
-  mesc_data_handling.tiff_merge(mesc_file_name, list_of_file_nums, root_directory)
+  mesc_data_handling.tiff_merge(mesc_file_name, list_of_file_nums, root_directory, True)
   #mesc_data_handling.extract_stim_frame(root_directory, mesc_DATA_file, list_of_file_nums) #--> saves stimTimes.npy needed for baseline
 if S2P:
   suite2p_script.run_suite2p(tiff_directory, list_of_file_nums, False, gcamp)
@@ -111,20 +129,18 @@ if S2P:
 #--------------Suite2p manual sorting------------------
 
 if RUN_ANALYSIS_PREP:
-  '''functions.stim_dur_val(tiff_directory, list_of_file_nums)
-  functions.electROI_val(tiff_directory, list_of_file_nums)
+  '''functions.electROI_val(tiff_directory, list_of_file_nums)'''
   functions.dist_vals(tiff_directory, list_of_file_nums)
-  functions.stim_dur_val(tiff_directory, list_of_file_nums)'''
+  functions.stim_dur_val(tiff_directory, list_of_file_nums)
+  #functions.activated_neurons_val(root_directory, tiff_directory, list_of_file_nums, 1)
+  functions.timecourse_vals(tiff_directory, list_of_file_nums, 5)
+  #functions.spontaneous_baseline_val(tiff_directory,list_of_file_nums, [2,3,5,6,8,9,0,1,10,11,12,13,15], frame_rate= 30.97, plot_start_frame = 0, plot_end_frame=None)
   #functions.baseline_val(root_directory, tiff_directory, list_of_file_nums) #--> saves F0.npy : if suite2p files changes make sure to rerun
-  functions.analyze_merged_activation_and_save(root_directory, mesc_file_name, tiff_directory, list_of_file_nums)
+  #functions.analyze_merged_activation_and_save(root_directory, mesc_file_name, tiff_directory, list_of_file_nums)
 
-
-#functions.activated_neurons_val(root_directory, tiff_directory, list_of_file_nums, 1)
-#functions.timecourse_vals(tiff_directory, list_of_file_nums, 5)
-#functions.data_analysis_values(stim_type, tiff_directory, list_of_file_nums)
-#functions_current_steering.plot_stim_traces(tiff_directory, num_repeats=6, num_stims_per_repeat=5)
 if PLOTS:
   functions.plot_stim_traces(tiff_directory, 30.97, 6, 5, list_of_file_nums, 8, 5.2, 2) #5.165
+  #functions.data_analysis_values(stim_type, tiff_directory, list_of_file_nums)
 if PLOT_BTW_EXP:
   functions.plot_across_experiments(root_directory, tiff_directory, list_of_file_nums, 30.97)
 
@@ -135,9 +151,12 @@ if RUN_CELLREG_PREP:
   cellreg_preprocess.suite2p_to_cellreg_masks(tiff_directory, list_of_file_nums)
 # manual: run cellreg pipeline
 if RUN_CELLREG_ANALYSIS:
-  #cellreg_preprocess.cellreg_analysis(tiff_directory, mat_file, list_of_file_nums, postfix, 5)
+  #cellreg_preprocess.cellreg_analysis_overlap(tiff_directory, mat_file, list_of_file_nums, postfix)
   #cellreg_preprocess.single_block_activation(tiff_directory,postfix, mat_file,  30.97, 10, list_of_file_nums, 2.4,200, 3 )
   cellreg_analysis.run_cellreg_matlab(tiff_directory)
+if VIDEO:
+  #functions.get_stim_frames_to_video(root_directory, tiff_directory,list_of_file_nums)
+  functions.create_video_from_mesc_tiffs(root_directory, list_of_file_nums)
 '''
 functions.stim_dur_val(tiff_directory, list_of_file_nums)
 functions.electROI_val(tiff_directory, list_of_file_nums)
@@ -147,33 +166,4 @@ functions.baseline_val(root_directory, tiff_directory, list_of_file_nums)
 functions.activated_neurons_val(root_directory, tiff_directory, list_of_file_nums)
 functions.timecourse_vals(tiff_directory, list_of_file_nums, 5)
 functions.data_analysis_values(stim_type, tiff_directory, list_of_file_nums)
-
-
-
-#TBD
-
-functions.scratch_val(tiff_directory)
-'''
-'''
-sum_avg_path = 'path/to/sum_avg_values.npy'
-            sum_avg_per_amplitude = np.load(sum_avg_path, allow_pickle=True).item()
-
-            # kell?
-            sorted_items = sorted(sum_avg_per_amplitude.items())
-
-            # time axis of 4s duration
-            trace_length = len(next(iter(sum_avg_per_amplitude.values())))
-            time = np.linspace(-1, 3, trace_length)
-
-            plt.figure(figsize=(8, 5))
-            for amplitude, trace in sorted_items:
-                plt.plot(time, trace, label=f"{amplitude} μA", linewidth=2)
-
-            plt.title("Sum Avg Traces per Amplitude: .npy mentes ell")
-            plt.xlabel("Time (s)")
-            plt.ylabel("Mean ΔF/F₀")
-            plt.legend()
-            plt.grid(True)
-            plt.tight_layout()
-            plt.show()
 '''

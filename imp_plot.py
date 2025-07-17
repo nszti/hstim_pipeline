7cee25ca551746fd068691dd620b6d5a2b8e93e2
fredrik <nguyen.eszter@gmail.com>
2025-07-17 14:23:48 +0200

    rms plot

--- imp_plot.py
+++ imp_plot.py
@@ -1,11 +1,14 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+import seaborn as sns
 from matplotlib.ticker import LogLocator, NullFormatter
 
 # Reload the Excel file
 file_path = 'c:/Hyperstim/2025_07_01/nanoz_m_imp.xlsx'
 df = pd.read_excel(file_path)
+
+
 
 # Extract frequencies and impedance data
 frequencies = []
@@ -58,3 +61,27 @@ plt.grid(False)
 plt.legend()
 plt.tight_layout()
 plt.show()
+
+# ======= rms boxplot =======
+'''
+df = pd.read_excel(file_path)
+
+# Create the plot
+plt.figure(figsize=(6, 6))
+ax = sns.boxplot(x='1-500', y='Noise (μV RMS)', data=df, showfliers=False, width=0.6)
+sns.stripplot(x='1-500', y='Noise (μV RMS)', data=df, color='black', size=4, jitter=True, alpha=0.5)
+
+
+grouped = df.groupby('1-500')['Noise (μV RMS)']
+positions = range(len(grouped))
+for pos, (label, values) in zip(positions, grouped):
+    mean = values.mean()
+    median = values.median()
+    plt.plot(pos, mean, 'ko', markersize=8)
+    plt.plot([pos - 0.2, pos + 0.2], [median, median], color='red', lw=2)
+
+plt.ylabel("Noise (μV$_{RMS}$)", fontsize=12)
+plt.title("B", loc='left', fontsize=14, weight='bold')
+sns.despine()
+plt.tight_layout()
+plt.show()'''
